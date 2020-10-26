9ac96f656d8499b7d4e126236d4b750b8bcc4768
Alex Scotton <alex@alexscotton.com>
2020-10-26 14:54:31 +0000

    Complete Field interface to proxy requests to each sub field

--- src/NovaEmojiFieldContainer.php
+++ src/NovaEmojiFieldContainer.php
@@ -28,4 +28,59 @@ class NovaEmojiFieldContainer extends Field
     {
         $this->withMeta(['fields' => $fields]);
     }
+
+    /**
+     * Resolve the field's value.
+     *
+     * @param mixed $resource
+     * @param string|null $attribute
+     * @return void
+     */
+    public function resolve($resource, $attribute = null) : void
+    {
+        foreach ($this->meta['fields'] as $field) {
+            $field->resolve($resource, $attribute);
+        }
+    }
+
+    /**
+     * Get the validation rules for this field.
+     *
+     * @param NovaRequest $request
+     * @return array
+     */
+    public function getRules(NovaRequest $request) : array
+    {
+        $rules = parent::getRules($request);
+
+        foreach ($this->meta['fields'] as $field) {
+            $rules = array_merge($rules, $field->getRules($request));
+        }
+
+        return $rules;
+    }
+
+    /**
+     * Hydrate the given attribute on the model based on the incoming request.
+     *
+     * @param NovaRequest $request
+     * @param Model $model
+     * @return void
+     */
+    public function fill(NovaRequest $request, $model) : void
+    {
+        foreach ($this->meta['fields'] as $field) {
+            $field->fill($request, $model);
+        }
+    }
+
+    /**
+     * Prepare the field for JSON serialization.
+     *
+     * @return array
+     */
+    public function jsonSerialize() : array
+    {
+        return array_merge([ 'fields' => $this->meta['fields'] ], parent::jsonSerialize());
+    }
 }
